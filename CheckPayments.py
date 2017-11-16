df407c031402614cab7b45faa2fc6ef30a3371f3
nathan <nathan.stern15@gmail.com>
2017-11-16 10:57:37 +0100

    possible CheckPayments fix

--- CheckPayments.py
+++ CheckPayments.py
@@ -9,18 +9,13 @@ json = json.loads(r.text)
 for item in json["_items"]:
 	amount = float(str(item["amount"]["amount"]))
 	memo = item["memo"]
-	try:
-		conn = psycopg2.connect((os.environ['DATABASE_URL']),sslmode='require')
-	except:
-	    print ("I am unable to connect to the database.")
+	conn = psycopg2.connect((os.environ['DATABASE_URL']),sslmode='require')
+	#conn = psycopg2.connect("dbname='my_database_development' user='nathan' host='127.0.0.1' password=''")
 	cur = conn.cursor()
-	try:
-		cur.execute("""UPDATE users SET steem = u.steem + %s FROM transactions t INNER JOIN users u ON t.id = u.transactionid WHERE t.name =%s""",(amount,memo,))
-		cur.execute("""DELETE FROM transactions WHERE name LIKE %s""",(memo,))
-		cur.close()
-		conn.commit()
-		conn.close()
-	except:
-	    print ("I can't exec")
+	print cur.execute("""UPDATE users SET steem = u.steem + %s FROM transactions t INNER JOIN users u ON t.id = u.transactionid WHERE t.name =%s""",(amount,memo,))
+	print cur.execute("""DELETE FROM transactions WHERE name LIKE %s""",(memo,))
+	cur.close()
+	conn.commit()
+	conn.close()
 
 #s.get_account_history('steemvisit',index_from=-1,limit=1)
