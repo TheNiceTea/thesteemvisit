deeeaa4aef781fab38ffd43947536dd3d98ec438
nathan <nathan.stern15@gmail.com>
2017-11-16 12:27:35 +0100

    Checkpayments.py - Steem API

--- CheckPayments.py
+++ CheckPayments.py
@@ -2,13 +2,18 @@ import requests
 import json
 import psycopg2
 import os
-
-
+from steem import Steem
+'''
 r = requests.get('https://webapi.steemdata.com/Operations?where={"type": "transfer", "to": "steemvisit"}')
 json = json.loads(r.text)
 for item in json["_items"]:
 	amount = float(str(item["amount"]["amount"]))
 	memo = item["memo"]
+'''
+s = Steem()
+for i in s.get_account_history('steemvisit',index_from=-1,limit=1):
+	memo = i[1]['op'][1]['memo']
+	amount = float(i[1]['op'][1]['amount'][:-3])
 	conn = psycopg2.connect((os.environ['DATABASE_URL']),sslmode='require')
 	#conn = psycopg2.connect("dbname='my_database_development' user='nathan' host='127.0.0.1' password=''")
 	cur = conn.cursor()
@@ -18,4 +23,3 @@ for item in json["_items"]:
 	conn.commit()
 	conn.close()
 
-#s.get_account_history('steemvisit',index_from=-1,limit=1)
